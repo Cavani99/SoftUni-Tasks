defe7a5ce2176ec2ad58daba6ff1126f0e70ef9e
Kristian <kristiian.donchev@abv.bg>
2023-09-20 17:47:47 +0300

    Data Types and Variables Task 8.Lower-Case Or Upper-Case letter

--- Main.java
+++ Main.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void  ReversedChars() {
+    public static void  LowerOrUpper() {
         Scanner sc = new Scanner(System.in);
 
+        char character= sc.nextLine().charAt(0);
 
-        char first= sc.nextLine().charAt(0);
-        char second= sc.nextLine().charAt(0);
-        char third= sc.nextLine().charAt(0);
-
-
-        System.out.printf("%c %c %c",third,second,first);
+       if((int) character>=65 && (int)character<=90){
+           System.out.println("upper-case");
+       }else
+           System.out.println("lower-case");
 
     }
 
 
     public static void main(String[] args) {
-        ReversedChars();
+        LowerOrUpper();
     }
 }
