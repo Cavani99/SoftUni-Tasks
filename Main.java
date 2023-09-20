40430065f4d05029a4841f06a724a7d4ae2ccb72
Kristian <kristiian.donchev@abv.bg>
2023-09-21 00:16:27 +0300

    Data Types and Variables Task 9.Centuries to Minutes

--- Main.java
+++ Main.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void  LowerOrUpper() {
+    public static void  CenturiestoMinutes() {
         Scanner sc = new Scanner(System.in);
 
-        char character= sc.nextLine().charAt(0);
+        int centuries=Integer.parseInt(sc.nextLine());
+        double years=centuries*100;
+        double days= years*365.2422;
+        double hours=days*24;
+        double minutes=hours*60;
 
-       if((int) character>=65 && (int)character<=90){
-           System.out.println("upper-case");
-       }else
-           System.out.println("lower-case");
+        System.out.printf("%d centuries = %.0f years = %.0f days " +
+                "= %.0f hours = %.0f minutes",centuries,years,days,hours,minutes);
 
     }
 
 
     public static void main(String[] args) {
-        LowerOrUpper();
+        CenturiestoMinutes();
     }
 }
