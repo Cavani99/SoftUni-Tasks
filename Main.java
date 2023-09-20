f80e40cc9ae4f1bb71d85334801a1ceb40f5438f
Kristian <kristiian.donchev@abv.bg>
2023-09-20 15:41:46 +0300

    Data Types and Variables Task 5.ConcatNames

--- Main.java
+++ Main.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void  TownInfo() {
+    public static void  ConcatNames() {
         Scanner sc = new Scanner(System.in);
 
-        String town=  sc.nextLine();
-        long population=Long.parseLong(sc.nextLine());
-        int area=Integer.parseInt(sc.nextLine());
+        String firstName= sc.nextLine();
+        String lastName= sc.nextLine();
+        String delimiter= sc.nextLine();
 
-        System.out.printf("Town %s has population of %d and area %d square km.",town,population,area);
-
+        System.out.println(firstName+delimiter+lastName);
     }
 
     public static void main(String[] args) {
-        TownInfo();
+        ConcatNames();
     }
 }
