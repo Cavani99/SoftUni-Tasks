4e1a48e5e2c312a89bf17f86120adcd568922fb1
Kristian <kristiian.donchev@abv.bg>
2023-09-22 14:39:04 +0300

    Data Types and Variables -Exercise Task 5.Print Part Of ASCII Table

--- Main.java
+++ Main.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void SumOfChars() {
+    public static void PrintPartOfASCIITable() {
         Scanner sc = new Scanner(System.in);
 
-        int characters = Integer.parseInt(sc.nextLine());
+        int indexStart = Integer.parseInt(sc.nextLine());
+        int indexEnd = Integer.parseInt(sc.nextLine());
 
-        int sum=0;
-        for(int i=1;i<=characters;i++){
-            char ch=sc.nextLine().charAt(0);
-            sum+=ch;
 
+        for(int i=indexStart;i<=indexEnd;i++){
+            char ch=(char)i;
+            System.out.print(ch + " ");
         }
-        System.out.printf("The sum equals: %d",sum);
+
 
     }
 
 
     public static void main(String[] args) {
-        SumOfChars();
+        PrintPartOfASCIITable();
     }
 }
