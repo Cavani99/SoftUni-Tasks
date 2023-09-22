fede806eff01d178ec6b2917c6add0184f3927b3
Kristian <kristiian.donchev@abv.bg>
2023-09-22 14:02:23 +0300

    Data Types and Variables -Exercise Task 2.Sum Digits

--- Main.java
+++ Main.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void IntegerOperations() {
+    public static void SumDigits() {
         Scanner sc = new Scanner(System.in);
 
-        int number1 = Integer.parseInt(sc.nextLine());
-        int number2 = Integer.parseInt(sc.nextLine());
-        int number3 = Integer.parseInt(sc.nextLine());
-        int number4 = Integer.parseInt(sc.nextLine());
+        int number = Integer.parseInt(sc.nextLine());
 
-        int sum=number1 + number2;
-        sum/=number3;
-        sum*=number4;
+        String input= String.valueOf(number);
+        int sum=0;
 
+        for(int i=0;i<=input.length()-1;i++){
+            int index=Integer.parseInt(String.valueOf(input.charAt(i)));
+            sum+=index;
+        }
         System.out.println(sum);
-
     }
 
 
     public static void main(String[] args) {
-        IntegerOperations();
+        SumDigits();
     }
 }
