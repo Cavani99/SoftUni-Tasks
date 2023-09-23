f53619388e52eefb5d792eb12d3639e7211ae3a0
Kristian <kristiian.donchev@abv.bg>
2023-09-23 19:15:34 +0300

    Data Types and Variables -Exercise Task 7.Water Overflow

--- Main.java
+++ Main.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void TriplesofLatinLetters() {
+    public static void WaterOverflow() {
         Scanner sc = new Scanner(System.in);
 
-        int letters = Integer.parseInt(sc.nextLine());
+        int litersOfWater = Integer.parseInt(sc.nextLine());
 
-        for(int i=0;i<letters;i++){
-            for(int j=0;j<letters;j++){
-                for(int k=0;k<letters;k++){
-                     char firstChar= (char) ('a'+ i);
-                     char secondChar= (char) ('a'+ j);
-                     char thirdChar= (char) ('a'+ k);
-                    System.out.printf("%c%c%c\n",firstChar,secondChar,thirdChar);
-                }
+        int amountFilled=0;
+
+        for(int i=1;i<=litersOfWater;i++){
+            int liters = Integer.parseInt(sc.nextLine());
+            amountFilled+=liters;
+
+            if(amountFilled>255) {
+                System.out.println("Insufficient capacity!");
+                amountFilled-=liters;
             }
-        }
 
+        }
+        System.out.println(amountFilled);
 
 
     }
 
 
     public static void main(String[] args) {
-        TriplesofLatinLetters();
+        WaterOverflow();
     }
 }
