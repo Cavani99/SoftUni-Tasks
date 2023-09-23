1e95af523ad464c6729dcd8b8c38a9c6cb7075a2
Kristian <kristiian.donchev@abv.bg>
2023-09-23 19:00:48 +0300

    Data Types and Variables -Exercise Task 6.Triples of Latin Letters

--- Main.java
+++ Main.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void PrintPartOfASCIITable() {
+    public static void TriplesofLatinLetters() {
         Scanner sc = new Scanner(System.in);
 
-        int indexStart = Integer.parseInt(sc.nextLine());
-        int indexEnd = Integer.parseInt(sc.nextLine());
+        int letters = Integer.parseInt(sc.nextLine());
 
-
-        for(int i=indexStart;i<=indexEnd;i++){
-            char ch=(char)i;
-            System.out.print(ch + " ");
+        for(int i=0;i<letters;i++){
+            for(int j=0;j<letters;j++){
+                for(int k=0;k<letters;k++){
+                     char firstChar= (char) ('a'+ i);
+                     char secondChar= (char) ('a'+ j);
+                     char thirdChar= (char) ('a'+ k);
+                    System.out.printf("%c%c%c\n",firstChar,secondChar,thirdChar);
+                }
+            }
         }
+
 
 
     }
 
 
     public static void main(String[] args) {
-        PrintPartOfASCIITable();
+        TriplesofLatinLetters();
     }
 }
