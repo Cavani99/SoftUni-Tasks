63f0c9308060592e92e5984eb9dccbea5589234f
Kristian <kristiian.donchev@abv.bg>
2023-09-23 19:29:45 +0300

    Data Types and Variables -Exercise Task 8.Beer Kegs

--- Main.java
+++ Main.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void WaterOverflow() {
+    public static void BeerKegs() {
         Scanner sc = new Scanner(System.in);
 
-        int litersOfWater = Integer.parseInt(sc.nextLine());
+        int beerKegs = Integer.parseInt(sc.nextLine());
 
-        int amountFilled=0;
+        String bestModel="";
+        double bestVolume=0;
+        for(int i=1;i<=beerKegs;i++){
+            String model=sc.nextLine();
+            double radius=Double.parseDouble(sc.nextLine());
+            int height=Integer.parseInt(sc.nextLine());
 
-        for(int i=1;i<=litersOfWater;i++){
-            int liters = Integer.parseInt(sc.nextLine());
-            amountFilled+=liters;
-
-            if(amountFilled>255) {
-                System.out.println("Insufficient capacity!");
-                amountFilled-=liters;
+            double volume= Math.PI* radius*radius *height;
+            if(volume>bestVolume){
+                bestModel=model;
+                bestVolume=volume;
             }
 
-        }
-        System.out.println(amountFilled);
 
+
+        }
+        System.out.println(bestModel);
 
     }
 
 
     public static void main(String[] args) {
-        WaterOverflow();
+        BeerKegs();
     }
 }
