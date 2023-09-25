b17ab559f20dc81db25d8ee83fc4c6ec55387cc6
Kristian <kristiian.donchev@abv.bg>
2023-09-25 12:35:44 +0300

    Data Types and Variables -More Exercise Task 3.Floating Equality

--- Main.java
+++ Main.java
@@ -1,58 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void FromLefttoTheRight() {
+    public static void FloatingEquality() {
         Scanner sc = new Scanner(System.in);
 
-        int linesOfInput=Integer.parseInt(sc.nextLine());
+        double number1=Double.parseDouble(sc.nextLine());
+        double number2=Double.parseDouble(sc.nextLine());
 
+        double eps=0.000001;
+        double diff= Math.abs(number1-number2);
+        if(eps>diff){
+            System.out.println("True");
+        }else
+            System.out.println("False");
 
-
-        for(int i=1;i<=linesOfInput;i++){
-
-            String input=sc.nextLine();
-            boolean firstNumberFinished=false;
-            String firstNumber="";
-            String secondNumber="";
-
-            for(int k=0;k<=input.length()-1;k++) {
-                char ch = input.charAt(k);
-                if (ch == ' ')
-                    firstNumberFinished = true;
-                else{
-                    if(!firstNumberFinished)
-                        firstNumber+=ch;
-                    else
-                        secondNumber+=ch;
-                }
-            }
-
-            long number1=  Long.parseLong(firstNumber);
-            long number2= Long.parseLong(secondNumber);
-            int sum=0;
-            if(number1>=number2){
-
-               while(number1!=0){
-                   sum+=number1 % 10;
-                   number1/=10;
-               }
-
-            } else {
-
-                while(number2!=0){
-                    sum+=number2 % 10;
-                    number2/=10;
-                }
-
-            }
-
-            System.out.println(Math.abs(sum));
-        }
 
     }
 
 
     public static void main(String[] args) {
-        FromLefttoTheRight();
+        FloatingEquality();
     }
 }
