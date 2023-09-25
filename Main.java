0e3a3dd11c6f7353806e49e9ad79a943830817b2
Kristian <kristiian.donchev@abv.bg>
2023-09-25 12:43:07 +0300

    Data Types and Variables -More Exercise Task 4.Refactoring: Prime Checker

--- Main.java
+++ Main.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void FloatingEquality() {
+    public static void PrimeChecker() {
         Scanner sc = new Scanner(System.in);
 
-        double number1=Double.parseDouble(sc.nextLine());
-        double number2=Double.parseDouble(sc.nextLine());
-
-        double eps=0.000001;
-        double diff= Math.abs(number1-number2);
-        if(eps>diff){
-            System.out.println("True");
-        }else
-            System.out.println("False");
+        int range = Integer.parseInt(sc.nextLine());
+        for (int number = 2; number <= range; number++) {
+            boolean isPrime = true;
+            for (int divider = 2; divider < number; divider++) {
+                if (number % divider == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            System.out.printf("%d -> %b%n", number, isPrime);
+        }
 
 
     }
 
 
     public static void main(String[] args) {
-        FloatingEquality();
+        PrimeChecker();
     }
 }
