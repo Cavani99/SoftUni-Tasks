4a44444327e38d3f2855112abf75da2b8374511c
Kristian <kristiian.donchev@abv.bg>
2023-09-25 12:51:10 +0300

    Data Types and Variables -More Exercise Task 5.Decrypting Message

--- Main.java
+++ Main.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void PrimeChecker() {
+    public static void DecryptingMessage() {
         Scanner sc = new Scanner(System.in);
 
-        int range = Integer.parseInt(sc.nextLine());
-        for (int number = 2; number <= range; number++) {
-            boolean isPrime = true;
-            for (int divider = 2; divider < number; divider++) {
-                if (number % divider == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            System.out.printf("%d -> %b%n", number, isPrime);
+        int key = Integer.parseInt(sc.nextLine());
+        int lines = Integer.parseInt(sc.nextLine());
+
+        String message="";
+        for(int i=1;i<=lines;i++){
+            char ch=sc.nextLine().charAt(0);
+            ch+=key;
+            message+=ch;
         }
+        System.out.println(message);
+
 
 
     }
 
 
     public static void main(String[] args) {
-        PrimeChecker();
+        DecryptingMessage();
     }
 }
