d1571bce2f5a592309b050cb67ad9274f18f3af9
Kristian <kristiian.donchev@abv.bg>
2023-09-27 19:51:48 +0300

    Arrays - Lab Task 6.Equal Arrays

--- Main.java
+++ Main.java
@@ -2,30 +2,38 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void EvenAndOddSubtraction() {
+    public static void EqualArrays() {
         Scanner sc = new Scanner(System.in);
 
-        int [] numbers= Arrays
+        int [] numberArray1= Arrays
                 .stream(sc.nextLine().split(" "))
                 .mapToInt(e -> Integer.parseInt(e))
                 .toArray();
 
-        int sumEven=0;
-        int sumOdd=0;
-        for (int number : numbers) {
-            if (number % 2 == 0)
-                sumEven += number;
-            else
-                sumOdd += number;
+        int [] numberArray2= Arrays
+                .stream(sc.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+
+        int sum=0;
+        boolean areDifferent=false;
+        for (int i=0;i<numberArray1.length;i++){
+            sum+=numberArray1[i];
+            if(numberArray1[i]!=numberArray2[i]){
+                System.out.printf("Arrays are not identical." +
+                        " Found difference at %d index.",i);
+                areDifferent=true;
+                break;
+            }
         }
-
-        System.out.println(sumEven-sumOdd);
-
+        if(!areDifferent){
+            System.out.printf("Arrays are identical. Sum: %d",sum);
+        }
 
     }
 
 
     public static void main(String[] args) {
-        EvenAndOddSubtraction();
+        EqualArrays();
     }
 }
