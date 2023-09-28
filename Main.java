508a04dc478a01086789fab72a7daf63ef41e315
Kristian <kristiian.donchev@abv.bg>
2023-09-28 20:06:52 +0300

    Arrays - Exercise Task 8.Magic Sum

--- Main.java
+++ Main.java
@@ -2,51 +2,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void MaxSequenceOfEqualElements() {
+    public static void MagicSum() {
         Scanner sc = new Scanner(System.in);
 
         int [] numbers= Arrays
                 .stream(sc.nextLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
+        int magicNumber=Integer.parseInt(sc.nextLine());
 
-        int startIndex=0;
-        int indexValue=0;
-        int longestCount=0;
-        int longestIndex=0;
-        int count=0;
-        for(int i=0;i< numbers.length;i++){
-            if(i==0){
-                indexValue=numbers[i];
-                count++;
+        for (int i=0;i<numbers.length;i++){
+            for(int k=i+1;k<numbers.length;k++){
+                if(numbers[i]+numbers[k]==magicNumber && k!=i)
+                    System.out.println(numbers[i]+" "+numbers[k]);
             }
-            else if(numbers[i]==indexValue){
-                count++;
-            }else {
-                if(count>longestCount){
-                    longestCount=count;
-                    longestIndex=startIndex;
-                }
-                startIndex=i;
-                indexValue=numbers[i];
-                count=1;
-            }
-
         }
 
-        if(count>longestCount){
-            longestCount=count;
-            longestIndex=startIndex;
-        }
 
-        for(int j=longestIndex;j<longestCount+longestIndex;j++){
-            System.out.print(numbers[j]+" ");
-        }
 
 
     }
 
     public static void main(String[] args) {
-        MaxSequenceOfEqualElements();
+        MagicSum();
     }
 }
