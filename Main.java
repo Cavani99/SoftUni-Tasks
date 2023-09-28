bdb36c420f2d6a138e6ccb524fca3cf65fb89710
Kristian <kristiian.donchev@abv.bg>
2023-09-28 14:50:40 +0300

    Arrays - Exercise Task 3.Zig Zag Arrays

--- Main.java
+++ Main.java
@@ -1,28 +1,44 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void CommonElements() {
+    public static void ZigZagArrays() {
         Scanner sc = new Scanner(System.in);
 
-        String [] arr1 =sc.nextLine().split(" ");
-        String [] arr2 =sc.nextLine().split(" ");
+        int indexes=Integer.parseInt(sc.nextLine());
 
+        int []arr1=new int[indexes];
+        int []arr2=new int[indexes];
 
-        for(int i=0;i<arr2.length;i++){
-            for(int k=0;k<arr1.length;k++){
-                if(arr2[i].equals(arr1[k])){
-                    System.out.print(arr2[i]+" ");
-                }
+        for(int i=0;i<indexes;i++){
+
+            int [] numbers= Arrays
+                    .stream(sc.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            if(i % 2==0){
+                arr1[i]=numbers[0];
+                arr2[i]=numbers[1];
+            }else{
+                arr1[i]=numbers[1];
+                arr2[i]=numbers[0];
             }
+
         }
 
+        for (int index:arr1){
+            System.out.print(index+" ");
+        }
+        System.out.println();
+        for (int index:arr2){
+            System.out.print(index+" ");
+        }
 
-
-         
     }
 
 
     public static void main(String[] args) {
-        CommonElements();
+        ZigZagArrays();
     }
 }
