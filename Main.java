f5bd1f2f3803aa4292dca61a965062cc044da73e
Kristian <kristiian.donchev@abv.bg>
2023-09-28 20:58:34 +0300

    Arrays - Exercise Task 9.Array Modifier

--- Main.java
+++ Main.java
@@ -2,28 +2,50 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void MagicSum() {
+    public static void ArrayModifier() {
         Scanner sc = new Scanner(System.in);
 
         int [] numbers= Arrays
                 .stream(sc.nextLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-        int magicNumber=Integer.parseInt(sc.nextLine());
 
-        for (int i=0;i<numbers.length;i++){
-            for(int k=i+1;k<numbers.length;k++){
-                if(numbers[i]+numbers[k]==magicNumber && k!=i)
-                    System.out.println(numbers[i]+" "+numbers[k]);
+        String [] input =sc.nextLine().split(" ");
+        while(!input[0].equals("end")){
+            if(input[0].equals("swap")){
+                int index1= Integer.parseInt(input[1]);
+                int index2= Integer.parseInt(input[2]);
+                int oldValue= numbers[index1];
+                numbers[index1]=numbers[index2];
+                numbers[index2]=oldValue;
+
             }
+            else if(input[0].equals("multiply")){
+                int index1= Integer.parseInt(input[1]);
+                int index2= Integer.parseInt(input[2]);
+                int mulipliedValue=numbers[index1]*numbers[index2];
+                numbers[index1]=mulipliedValue;
+            }else if(input[0].equals("decrease")){
+                for(int i=0;i<numbers.length;i++){
+                    numbers[i]-=1;
+                }
+            }
+
+
+            input =sc.nextLine().split(" ");
         }
 
+        String []arr= new String[]{Arrays.toString(numbers)
+                .replace("[", "")
+                .replace("]", "")};
+
+        System.out.println(String.join(", ", arr));
 
 
 
     }
 
     public static void main(String[] args) {
-        MagicSum();
+        ArrayModifier();
     }
 }
