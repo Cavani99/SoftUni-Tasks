e64e28a8a54aa2102a3bc0455f4d31cf40c4055b
Kristian <kristiian.donchev@abv.bg>
2023-10-01 13:36:58 +0300

    Arrays - More Exercise Task 3.Recursive Fibonacci

--- Main.java
+++ Main.java
@@ -1,32 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void PascalTriangle() {
-        Scanner sc = new Scanner(System.in);
-
-        int rows=Integer.parseInt(sc.nextLine());
-
-        int []arrValue=new int[rows];
-        for(int i=1;i<=rows;i++){
-            int []arr=new int[i];
-            for(int j=0;j<arr.length;j++){
-                if(j==0)
-                    arr[j]=1;
-                else
-                    arr[j]=arrValue[j]+arrValue[j-1];
+    public static int GetFibonacci(long n) {
+            if(n==1 || n==2)
+                return 1;
+            else{
+                return GetFibonacci(n-1)+GetFibonacci(n-2);
             }
-            for (int index:arr) {
-                System.out.print(index+" ");
-            }
-            System.out.println();
-
-            System.arraycopy(arr, 0, arrValue, 0, arr.length);
-        }
-
 
     }
 
     public static void main(String[] args) {
-        PascalTriangle();
+        Scanner sc = new Scanner(System.in);
+        long n=Integer.parseInt(sc.nextLine());
+
+        System.out.println(GetFibonacci(n));
     }
 }
