3226645e8d08902c932010d84cd9812384a2e767
Kristian <kristiian.donchev@abv.bg>
2023-10-04 14:47:08 +0300

    Methods - Lab Task 5.Orders

--- Main.java
+++ Main.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void add(int num1,int num2) {
-        System.out.println(num1+num2);
-
-    }
-    public static void multiply(int num1,int num2) {
-        System.out.println(num1*num2);
-
-    }
-    public static void subtract(int num1,int num2) {
-        System.out.println(num1-num2);
-
-    }
-    public static void divide(int num1,int num2) {
-        System.out.println(num1/num2);
+    public static void order(String product,int quantity) {
+       switch (product){
+           case "coffee":
+               System.out.printf("%.2f",quantity*1.50);
+               break;
+           case "water":
+               System.out.printf("%.2f",quantity*1.00);
+               break;
+           case "coke":
+               System.out.printf("%.2f",quantity*1.40);
+               break;
+           case "snacks":
+               System.out.printf("%.2f",quantity*2.00);
+               break;
+       }
 
     }
 
@@ -22,16 +23,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String operation=sc.nextLine();
-        int number1=Integer.parseInt(sc.nextLine());
-        int number2=Integer.parseInt(sc.nextLine());
+        String product=sc.nextLine();
+        int quantity=Integer.parseInt(sc.nextLine());
 
-        switch (operation) {
-            case "add" -> add(number1, number2);
-            case "multiply" -> multiply(number1, number2);
-            case "subtract" -> subtract(number1, number2);
-            case "divide" -> divide(number1, number2);
-        }
+        order(product,quantity);
+
+
 
     }
 }
