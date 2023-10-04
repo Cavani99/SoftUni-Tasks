68d96bb10a1db571cc9639b13a9b248885843124
Kristian <kristiian.donchev@abv.bg>
2023-10-04 14:39:45 +0300

    Methods - Lab Task 4.Calculations

--- Main.java
+++ Main.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void printingTriangle(int size) {
+    public static void add(int num1,int num2) {
+        System.out.println(num1+num2);
 
-    //print the upper and middle part of the triangle
-       for(int i=1;i<=size;i++){
-          for(int k=1;k<=i;k++){
-              System.out.print(k+" ");
-          }
-          System.out.println();
-       }
+    }
+    public static void multiply(int num1,int num2) {
+        System.out.println(num1*num2);
 
-    //print the bottom part of the triangle
-        for(int i=size-1;i>=1;i--){
-            for(int k=1;k<=i;k++){
-                System.out.print(k+" ");
-            }
-            System.out.println();
-        }
+    }
+    public static void subtract(int num1,int num2) {
+        System.out.println(num1-num2);
+
+    }
+    public static void divide(int num1,int num2) {
+        System.out.println(num1/num2);
 
     }
 
@@ -25,8 +22,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size=Integer.parseInt(sc.nextLine());
+        String operation=sc.nextLine();
+        int number1=Integer.parseInt(sc.nextLine());
+        int number2=Integer.parseInt(sc.nextLine());
 
-        printingTriangle(size);
+        switch (operation) {
+            case "add" -> add(number1, number2);
+            case "multiply" -> multiply(number1, number2);
+            case "subtract" -> subtract(number1, number2);
+            case "divide" -> divide(number1, number2);
+        }
+
     }
 }
