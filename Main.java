e66f6711e455b51a41629e7337fee0b5dfd1617f
Kristian <kristiian.donchev@abv.bg>
2023-10-04 19:47:18 +0300

    Methods - Lab Task 11. Math Operations

--- Main.java
+++ Main.java
@@ -1,52 +1,36 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
 
-    public static int getSumOfEvens(int n){
-        int sum=0;
+    public static double calculate(int num1,char operation,int num2){
 
-        String number=String.valueOf(n);
-        for(int i=0;i<number.length();i++){
-            if(number.charAt(i)=='-')
-                i++;
-            int numberToCompare=Integer.parseInt(String.valueOf(number.charAt(i)));
-            if(numberToCompare % 2==0 )
-                sum+=numberToCompare;
+
+        switch (operation){
+            case'/':
+                return num1/num2;
+            case'*':
+                return num1*num2;
+            case'+':
+                return num1+num2;
+            case'-':
+                return num1-num2;
+
 
         }
 
-        return sum;
+        return 0;
     }
-    public static int getSumOfOdds(int n){
-        int sum=0;
-
-        String number=String.valueOf(n);
-        for(int i=0;i<number.length();i++){
-            if(number.charAt(i)=='-')
-                i++;
-            int numberToCompare=Integer.parseInt(String.valueOf(number.charAt(i)));
-            if(numberToCompare % 2!=0 )
-                sum+=numberToCompare;
-
-        }
-
-        return sum;
-    }
-
-    public static int getMultipleOfEvensAndOdds(int number) {
-
-
-
-       return getSumOfEvens(number)*getSumOfOdds(number);
-    }
-
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number=Integer.parseInt(sc.nextLine());
+        int number1=Integer.parseInt(sc.nextLine());
+        char operator=sc.nextLine().charAt(0);
+        int number2=Integer.parseInt(sc.nextLine());
 
-        System.out.println(getMultipleOfEvensAndOdds(number));
+
+        System.out.println(new DecimalFormat("0.##").format(calculate(number1,operator,number2)));
 
     }
 }
