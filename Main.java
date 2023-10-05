3a8d71bb3244082565dfd6e8cf927b74284fa6c8
Kristian <kristiian.donchev@abv.bg>
2023-10-05 14:14:19 +0300

    Methods - Exercise Task 01. Smallest of Three Numbers

--- Main.java
+++ Main.java
@@ -1,36 +1,25 @@
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
 
-    public static double calculate(int num1,char operation,int num2){
+    public static int getSmallest(int num1,int num2,int num3){
+
+        int min=Math.min(num1,num2);
 
 
-        switch (operation){
-            case'/':
-                return num1/num2;
-            case'*':
-                return num1*num2;
-            case'+':
-                return num1+num2;
-            case'-':
-                return num1-num2;
-
-
-        }
-
-        return 0;
+        return Math.min(min,num3);
     }
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number1=Integer.parseInt(sc.nextLine());
-        char operator=sc.nextLine().charAt(0);
         int number2=Integer.parseInt(sc.nextLine());
+        int number3=Integer.parseInt(sc.nextLine());
+
+        System.out.println(getSmallest(number1,number2,number3));
 
 
-        System.out.println(new DecimalFormat("0.##").format(calculate(number1,operator,number2)));
 
     }
 }
