fd039bb9b454c098551b1a72880f870dcdd51c7e
Kristian <kristiian.donchev@abv.bg>
2023-10-05 14:45:08 +0300

    Methods - Exercise Task 04. Password Validator

--- Main.java
+++ Main.java
@@ -2,29 +2,58 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static String getCharactersRange(char ch1,char ch2){
-         String charactersInBetween="";
+    public static void passwordValidity(String password){
 
-         if(ch2>ch1) {
-             for (int i = (int) ch1 + 1; i < (int) ch2; i++) {
-                 charactersInBetween += (char) i + " ";
-             }
-         }else{
-             for (int i = (int) ch2 + 1; i < (int) ch1; i++) {
-                 charactersInBetween += (char) i + " ";
-             }
-         }
+        if(!validLength(password))
+            System.out.println("Password must be between 6 and 10 characters");
+        if(!onlyDigitsAndLetters(password))
+            System.out.println("Password must consist only of letters and digits");
+        if(!digitsMoreThanOne(password))
+            System.out.println("Password must have at least 2 digits");
 
-        return charactersInBetween;
+        if(validLength(password) && onlyDigitsAndLetters(password) && digitsMoreThanOne(password))
+            System.out.println("Password is valid");
     }
+    public static boolean validLength(String password){
+
+        return password.length() >= 6 && password.length() <= 10;
+
+    }
+    public static boolean onlyDigitsAndLetters(String password){
+
+        for(int i=0;i<password.length();i++){
+            char ch=password.charAt(i);
+            if((int) ch <=57 && (int) ch >=48 ||
+                    (int) ch >=65 && (int) ch <=90 ||
+            (int) ch >=97 && (int) ch <=122);
+            else return false;
+        }
+        return true;
+
+    }
+
+    public static boolean digitsMoreThanOne(String password){
+        int count=0;
+
+        for(int i=0;i<password.length();i++){
+            char ch=password.charAt(i);
+            if((int) ch <=57 && (int) ch >=48)
+                count++;
+            if(count>=2)
+                return true;
+
+        }
+        return false;
+
+    }
+
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char input1=sc.nextLine().charAt(0);
-        char input2=sc.nextLine().charAt(0);
+        String input=sc.nextLine();
 
-        System.out.println(getCharactersRange(input1,input2));
+        passwordValidity(input);
 
 
     }
