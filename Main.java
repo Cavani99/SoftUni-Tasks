903ae8c1763a7a662632a85ff2f489b957a84922
Kristian <kristiian.donchev@abv.bg>
2023-10-05 14:52:03 +0300

    Methods - Exercise Task 05. Add and Subtract

--- Main.java
+++ Main.java
@@ -2,59 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void passwordValidity(String password){
+    public static int sumTwoNumbers(int num1,int num2) {
 
-        if(!validLength(password))
-            System.out.println("Password must be between 6 and 10 characters");
-        if(!onlyDigitsAndLetters(password))
-            System.out.println("Password must consist only of letters and digits");
-        if(!digitsMoreThanOne(password))
-            System.out.println("Password must have at least 2 digits");
-
-        if(validLength(password) && onlyDigitsAndLetters(password) && digitsMoreThanOne(password))
-            System.out.println("Password is valid");
+        return num1+num2;
     }
-    public static boolean validLength(String password){
+    public static int subtractTwoNumbers(int num1,int num2,int num3) {
+        int sum=sumTwoNumbers(num1,num2);
 
-        return password.length() >= 6 && password.length() <= 10;
+        return sum-num3;
 
     }
-    public static boolean onlyDigitsAndLetters(String password){
-
-        for(int i=0;i<password.length();i++){
-            char ch=password.charAt(i);
-            if((int) ch <=57 && (int) ch >=48 ||
-                    (int) ch >=65 && (int) ch <=90 ||
-            (int) ch >=97 && (int) ch <=122);
-            else return false;
-        }
-        return true;
-
-    }
-
-    public static boolean digitsMoreThanOne(String password){
-        int count=0;
-
-        for(int i=0;i<password.length();i++){
-            char ch=password.charAt(i);
-            if((int) ch <=57 && (int) ch >=48)
-                count++;
-            if(count>=2)
-                return true;
-
-        }
-        return false;
-
-    }
-
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input=sc.nextLine();
+        int num1=Integer.parseInt(sc.nextLine());
+        int num2=Integer.parseInt(sc.nextLine());
+        int num3=Integer.parseInt(sc.nextLine());
 
-        passwordValidity(input);
-
+        System.out.println(subtractTwoNumbers(num1,num2,num3));
 
     }
 }
