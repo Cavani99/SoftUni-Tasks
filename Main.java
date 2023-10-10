91eebb7c6488f01f7da6e8328c5da9b6157d681c
Kristian <kristiian.donchev@abv.bg>
2023-10-10 22:30:17 +0300

    Bitwise Operations5. Odd Times

--- Main.java
+++ Main.java
@@ -1,33 +1,33 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 
-    public static void setBitAtNTo0(){
+    public static void ElementOccuringOddTimesUsingXOR(){
         Scanner sc = new Scanner(System.in);
-        int number=Integer.parseInt(sc.nextLine());
-        int n=Integer.parseInt(sc.nextLine());
+        int [] arr= Arrays
+                .stream(sc.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-
-        StringBuilder num= new StringBuilder(Integer.toBinaryString(number));
-
-        int count=0;
-        int index=0;
-        for(int i=num.length()-1;i>=0;i--){
-            if(count==n){
-                index=i;
+        int result=0;
+        for(int i=0;i<arr.length;i++){
+            result=result ^ arr[i];
+            for(int j=i+1;j< arr.length;j++){
+                if(arr[j]==arr[i])
+                    result=result ^ arr[j];
+            }
+            if(result!=0){
+                System.out.println(arr[i]);
                 break;
             }
-            count++;
+            result=0;
+
         }
 
-        num.setCharAt(index,'0');
-
-        String value= String.valueOf(num);
-        number=Integer.parseInt(value,2);
 
 
-        System.out.println(number);
 
     }
 
@@ -36,7 +36,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        setBitAtNTo0();
+        ElementOccuringOddTimesUsingXOR();
 
 
     }
