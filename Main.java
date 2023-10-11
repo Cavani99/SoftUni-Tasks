f4eb1ac607a21989618ae474f0aa4c2ad70477fa
Kristian <kristiian.donchev@abv.bg>
2023-10-11 16:01:48 +0300

    Lists - Lab 3. Merging Lists

--- Main.java
+++ Main.java
@@ -7,23 +7,48 @@ public class Main {
     public static void SumEqualNumbersInList(){
         Scanner sc = new Scanner(System.in);
 
-        List<Integer> numbers=
+        List<Integer> numbers1=
                 Arrays.stream(sc.nextLine().split(" "))
                         .map(Integer::parseInt).collect(Collectors.toList());
 
-        int loops = numbers.size() / 2 ;
+        List<Integer> numbers2=
+                Arrays.stream(sc.nextLine().split(" "))
+                        .map(Integer::parseInt).collect(Collectors.toList());
 
 
-        for(int i=0;i<loops;i++){
+        int loops = numbers1.size()+numbers2.size();
+        List <Integer> mergeList = new ArrayList<>(loops);
 
-               numbers.set(i, numbers.get(i)+ numbers.get(numbers.size()-1));
-               numbers.remove(numbers.size()-1);
+        while(mergeList.size()<loops)
+            mergeList.add(0);
 
+        boolean positive=false;
+        int index=0;
+        for(int i=0;i<mergeList.size();i++){
+
+            if(index<numbers1.size() && !positive){
+                mergeList.set(i, numbers1.get(index));
+                positive=true;
+            }
+            else if(index<numbers2.size() && positive) {
+                mergeList.set(i, numbers2.get(index));
+                positive=false;
+                index++;
+            }
+            else if(index>= numbers1.size()){
+                mergeList.set(i, numbers2.get(index));
+                index++;
+            }
+            else{
+                index++;
+                mergeList.set(i, numbers1.get(index));
+            }
         }
 
 
 
-        printElements(numbers, " ");
+
+        printElements(mergeList, " ");
     }
 
     public static void printElements(List <Integer> items,String delimiter){
