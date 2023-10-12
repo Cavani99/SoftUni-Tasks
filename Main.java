c51b920c926523d7c9130416a6daa85ad0bfa5d9
Kristian <kristiian.donchev@abv.bg>
2023-10-12 23:28:21 +0300

    Lists - Exercise 5. Bomb Numbers

--- Main.java
+++ Main.java
@@ -11,70 +11,56 @@ public class Main {
                 Arrays.stream(sc.nextLine().split(" "))
                         .map(Integer::parseInt).collect(Collectors.toList());
 
-        String [] command=sc.nextLine().split(" ");
-        while(!command[0].equals("End")){
+        List<Integer> bomb=
+                Arrays.stream(sc.nextLine().split(" "))
+                        .map(Integer::parseInt).collect(Collectors.toList());
+      
+        int bombValue=bomb.get(0);
+        int bombRadius=bomb.get(1);
 
-            if(command[0].equals("Add")){
-                int value= Integer.parseInt(command[1]);
-                elements.add(value);
-            }else if(command[0].equals("Insert")){
-                int value= Integer.parseInt(command[1]);
-                int index=Integer.parseInt(command[2]);
-                if(index>= elements.size() || index<0)
-                    System.out.println("Invalid index");
-                else
-                    insertElement(elements,value,index);
-            }else if(command[0].equals("Remove")){
-                int index=Integer.parseInt(command[1]);
-                if(index>= elements.size() || index<0)
-                    System.out.println("Invalid index");
-                else
-                    elements.remove(index);
-            }else if(command[0].equals("Shift")){
-                String direction=command[1];
-                int count=Integer.parseInt(command[2]);
 
-                shiftNumber(elements,direction,count);
+        detonation(elements,bombValue,bombRadius);
+
+
+        System.out.println(getSum(elements));
+    }
+
+    public static void detonation(List <Integer> elements,int bomb,int radius){
+        int bombIndex;
+
+        for (int i=0;i< elements.size();i++) {
+            if(elements.get(i)==bomb) {
+                bombIndex=i;
+                int indexToRemove=bombIndex-radius;
+                int amount=bombIndex-radius;
+
+                if(indexToRemove<0){
+                    indexToRemove=0;
+                    amount=0;
+                }
+
+                while(amount<radius+bombIndex+1 ){
+                    if(amount>0 || amount<elements.size()){
+                        if(elements.size()==indexToRemove || indexToRemove<0)
+                            break;
+                    elements.remove(indexToRemove);
+                    }
+                    amount++;
+                }
+                i=-1;
             }
 
-
-            command=sc.nextLine().split(" ");
         }
-
-
-
-
-        printElements(elements);
-    }
-
-    public static void insertElement(List <Integer> elements,int value,int index){
-
-        elements.add(index,value);
 
     }
 
-    public static void shiftNumber(List <Integer> elements,String direction,int count){
-
-        if(direction.equals("left")) {
-            Collections.rotate(elements,-count);
-
-        }
-        else if(direction.equals("right")) {
-            Collections.rotate(elements,count);
+    public static int getSum(List <Integer> elements){
+        int sum=0;
+        for (int i=0;i< elements.size();i++) {
+            sum+=elements.get(i);
         }
 
-
-    }
-
-
-    public static void printElements(List <Integer> items){
-
-
-        for (Integer item:items) {
-            System.out.print(item + " ");
-        }
-
-
+        return sum;
     }
 
 
