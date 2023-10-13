6f7c3dc81a965edc208ada9715c21c0eca6e8c28
Kristian <kristiian.donchev@abv.bg>
2023-10-13 23:52:20 +0300

    Lists - Exercise 7. Append Arrays

--- Main.java
+++ Main.java
@@ -4,77 +4,37 @@ import java.util.stream.Collectors;
 public class Main {
 
 
-    public static void listOperations(){
+    public static void sortSeveralArrays() {
         Scanner sc = new Scanner(System.in);
 
-        List<Integer> hand1=
-                Arrays.stream(sc.nextLine().split(" "))
-                        .map(Integer::parseInt).collect(Collectors.toList());
+        List<String> arr =
+                Arrays.stream(sc.nextLine().split("\\|"))
+                        .collect(Collectors.toList());
 
-        List<Integer> hand2=
-                Arrays.stream(sc.nextLine().split(" "))
-                        .map(Integer::parseInt).collect(Collectors.toList());
 
-        cardFight(hand1,hand2);
 
-        if(hand1.isEmpty())
-            System.out.printf("Second player wins! Sum: %d",getSum(hand2));
-        else
-            System.out.printf("First player wins! Sum: %d",getSum(hand1));
 
-    }
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            String []splitArray=arr.get(i).split("\\s+");
 
-    public static void cardFight(List <Integer> hand1,List <Integer> hand2){
-
-        while(true) {
-            int i = 0;
-
-            int card1 = hand1.get(i);
-            int card2 = hand2.get(i);
-
-            if (card1 > card2) {
-                hand2.remove(i);
-                hand1.remove(i);
-                hand1.add(card1);
-                hand1.add(card2);
-            } else if (card2 > card1) {
-                hand1.remove(i);
-                hand2.remove(i);
-                hand2.add(card2);
-                hand2.add(card1);
-            } else {
-                hand1.remove(i);
-                hand2.remove(i);
+            for(int j=0;j<splitArray.length;j++){
+                String value=splitArray[j];
+                if(!value.equals(""))
+                    System.out.print(value.trim()+" ");
             }
 
-
-            if (hand2.isEmpty()){
-                break;
-            }
-
-            else if (hand1.isEmpty()){
-                break;
-            }
         }
-
     }
 
-    public static int getSum(List <Integer> elements){
 
-        int sum=0;
-        for (int i=0;i< elements.size();i++) {
-            sum+=elements.get(i);
-        }
 
-        return sum;
-    }
 
 
 
 
     public static void main(String[] args) {
 
-        listOperations();
+        sortSeveralArrays();
 
 
     }
