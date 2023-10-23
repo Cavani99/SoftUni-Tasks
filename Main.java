9dbe300abd041d5e8513cba480ef72856e88c612
Kristian <kristiian.donchev@abv.bg>
2023-10-23 14:22:34 +0300

    Objects and Classes - Lab 3. Big Factorial

--- Main.java
+++ Main.java
@@ -4,21 +4,29 @@ import java.math.BigInteger;
 public class Main {
 
 
-    public static void SumBigNumbers() {
+    public static void BigFactorial() {
         Scanner sc = new Scanner(System.in);
 
-        BigInteger num1=new BigInteger(sc.nextLine());
-        BigInteger num2=new BigInteger(sc.nextLine());
+        int number=Integer.parseInt(sc.nextLine());
 
+        BigInteger sum= BigInteger.valueOf(1);
+        for (int i = 1; i <= number; i++) {
+            if(i==1)
+                sum=sum.multiply(BigInteger.valueOf(1));
+            else
+                sum=sum.multiply(BigInteger.valueOf(i));
 
-        System.out.println(num1.add(num2));
+        }
+
+        System.out.println(sum);
+
 
     }
 
 
     public static void main(String[] args) {
 
-        SumBigNumbers();
+        BigFactorial();
 
 
     }
