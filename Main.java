606c7bd4d160a8f59d4cd0bb94ba82dd3813accb
Kristian <kristiian.donchev@abv.bg>
2023-10-23 13:59:03 +0300

    Objects and Classes - Lab 1. Randomize Words

--- Main.java
+++ Main.java
@@ -1,74 +1,30 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
 
-    public static double budget;
-    public static void sortLists() {
+    public static void RandomizeWords() {
         Scanner sc = new Scanner(System.in);
 
+        String [] input=sc.nextLine().split(" ");
+        Random random=new Random();
 
-        budget=Double.parseDouble(sc.nextLine());
-
-        List<Integer> drums =
-                Arrays.stream(sc.nextLine().split(" "))
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toList());
-
-        List<Integer> copy = new ArrayList<>(drums);
-
-        String input=sc.nextLine();
-
-        while(!input.equals("Hit it again, Gabsy!")){
-            int hitPower=Integer.parseInt(input);
-            hitDrums(drums,copy,hitPower);
-
-            input=sc.nextLine();
+        for (int i = 0; i < input.length; i++) {
+            int index= random.nextInt(input.length);
+            String value=input[i];
+            input[i]=input[index];
+            input[index]=value;
         }
 
-        printElements(copy,budget);
+        System.out.println(String.join(System.lineSeparator(),input));
 
-    }
-
-
-    public static void hitDrums(List <Integer> initial,List<Integer> drums, int hitPower){
-
-        for (int i = 0; i < drums.size(); i++) {
-            drums.set(i, drums.get(i)-hitPower);
-
-            if(drums.get(i)<=0 && drums.get(i)!=-1000-hitPower){
-                double newBudget=budget;
-                newBudget-=initial.get(i)*3;
-
-                if(newBudget>=0){
-                    budget=newBudget;
-                    drums.set(i,initial.get(i));
-                }else{
-                    drums.set(i,-1000);
-                    i-=1;
-                }
-
-            }
-        }
-
-
-    }
-
-    public static void printElements(List <Integer> items,double budget) {
-
-        for (Integer item : items) {
-            if(item>0)
-                System.out.print(item + " ");
-        }
-        System.out.printf("\nGabsy has %.2flv.",budget);
 
     }
 
 
     public static void main(String[] args) {
 
-        sortLists();
+        RandomizeWords();
 
 
     }
