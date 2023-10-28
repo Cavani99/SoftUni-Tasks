6057614a8ace436acd39f97f9985cd74823abc86
Kristian <kristiian.donchev@abv.bg>
2023-10-28 14:06:43 +0300

    Objects and Classes - More Exercise 2. Raw Data

---
+++ Cargo.java
@@ -0,0 +1,27 @@
+public class Cargo {
+
+    private int cargoWeight;
+
+    public int getCargoWeight() {
+        return cargoWeight;
+    }
+
+    public void setCargoWeight(int cargoWeight) {
+        this.cargoWeight = cargoWeight;
+    }
+
+    public String getCargoType() {
+        return cargoType;
+    }
+
+    public void setCargoType(String cargoType) {
+        this.cargoType = cargoType;
+    }
+
+    private String cargoType;
+
+    public Cargo(int cargoWeight,String cargoType){
+        this.cargoWeight=cargoWeight;
+        this.cargoType=cargoType;
+    }
+}
--- Main.java
+++ Main.java
@@ -3,10 +3,50 @@ import java.util.*;
 public class Main {
 
 
-
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int number=Integer.parseInt(sc.nextLine());
+        List<Car> cars=new ArrayList<>();
+        for (int i = 1; i <=number ; i++) {
+            String [] input=sc.nextLine().split(" ");
+
+            int engineSpeed= Integer.parseInt(input[1]);
+            int enginePower= Integer.parseInt(input[2]);
+
+            int cargoWeight= Integer.parseInt(input[3]);
+            String cargoType=input[4];
+
+            double tire1Press=Double.parseDouble(input[5]);
+            int tir1Age=Integer.parseInt(input[6]);
+            double tire2Press=Double.parseDouble(input[7]);
+            int tir2Age=Integer.parseInt(input[8]);
+            double tire3Press=Double.parseDouble(input[9]);
+            int tir3Age=Integer.parseInt(input[10]);
+            double tire4Press=Double.parseDouble(input[11]);
+            int tir4Age=Integer.parseInt(input[12]);
+
+            Car car=new Car(input[0],new Engine(engineSpeed,enginePower)
+                    ,new Cargo(cargoWeight,cargoType),new Tire(tire1Press,tir1Age,tire2Press,tir2Age,tire3Press,tir3Age,tire4Press,tir4Age));
+
+            cars.add(car);
+        }
+
+        String type=sc.nextLine();
+        for (Car car:cars) {
+            if(type.equals("fragile")){
+                if(car.getCargo().getCargoType().equals(type)
+                        && car.getTire().getTire1Pressure()<1 || car.getTire().getTire2Pressure()<1
+                        || car.getTire().getTire3Pressure()<1 || car.getTire().getTire4Pressure()<1){
+                    System.out.println(car.getModel());
+                }
+            } else if (type.equals("flamable")) {
+                if(car.getCargo().getCargoType().equals(type) &&
+                        car.getEngine().getEnginePower()>250){
+                    System.out.println(car.getModel());
+                }
+            }
+        }
 
     }
 }
