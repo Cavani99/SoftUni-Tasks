5201f6264bf4370283efc74369868d46f41fc452
Kristian <kristiian.donchev@abv.bg>
2023-10-31 14:54:27 +0200

    Maps, Lambda and Stream API - Lab 4. Word Filter

--- Main.java
+++ Main.java
@@ -6,26 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String[] words=sc.nextLine().split(" ");
-
-        LinkedHashMap<String,Integer>counts=new LinkedHashMap<>();
-
-        for(String element:words){
-
-            if(!counts.containsKey(element.toLowerCase()))
-                counts.put(element.toLowerCase(),1);
-            else
-                counts.put(element.toLowerCase(),counts.get(element.toLowerCase())+1);
-
+        String[] words= Arrays.stream(sc.nextLine().split(" "))
+                .filter(w ->w.length() % 2 == 0)
+                .toArray(String[]::new);
+        for (String el:words) {
+            System.out.println(el);
         }
-
-        ArrayList <String> print=new ArrayList<>();
-        for(Map.Entry<String,Integer>entry: counts.entrySet()){
-            if(entry.getValue() %2!=0)
-                print.add(entry.getKey());
-        }
-
-        System.out.println(print.toString() .replace("[","").replace("]",""));
 
 
     }
