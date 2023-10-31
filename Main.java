3e131a4a894980b0e519301694360912c8356a54
Kristian <kristiian.donchev@abv.bg>
2023-10-31 13:49:00 +0200

    Maps, Lambda and Stream API - Lab 1. Count Real Numbers

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class Main {
@@ -6,98 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //make teams
-        int number=Integer.parseInt(sc.nextLine());
-        List <Team> teams=new ArrayList<>();
-        List <String> people=new ArrayList<>();
-        for (int i = 1; i <= number ; i++) {
-            String []info=sc.nextLine().split("-");
+        double [] nums=Arrays.stream(sc.nextLine().split(" "))
+                .mapToDouble(Double::parseDouble)
+                .toArray();
 
-            boolean Exists=false;
-            for (Team team:teams) {
-                if(team.getName().equals(info[1])){
-                    System.out.printf("Team %s was already created!\n",info[1]);
-                    Exists=true;
-                    break;
-                }else if(team.getCreator().equals(info[0])){
-                    System.out.printf("%s cannot create another team!\n",info[0]);
-                    Exists=true;
-                    break;
-                }
+        TreeMap<Double,Integer>counts=new TreeMap<>();
+        for (double num:nums) {
+
+            if(!counts.containsKey(num)){
+                counts.put(num,0);
             }
-
-            if(Exists)
-                continue;
-
-            Team team=new Team(info[0],info[1]);
-            System.out.printf("Team %s has been created by %s!\n",team.getName(),team.getCreator());
-
-            people.add(team.getCreator());
-            teams.add(team);
+            counts.put(num,counts.get(num)+1);
         }
 
-        //add members
-        String []command=sc.nextLine().split("->");
-        while(!command[0].equals("end of assignment")){
-
-            boolean teamExists=false;
-            boolean personExists=false;
-            int index=0;
-            for (Team team:teams) {
-                if (team.getName().equals(command[1])) {
-                    teamExists = true;
-                    index= teams.indexOf(team);
-                    break;
-                }
-            }
-
-            for (String person:people) {
-                 if(person.equals(command[0])){
-                     personExists=true;
-                     break;
-                 }
-            }
-
-
-            if(teamExists && !personExists){
-                teams.get(index).addMember(command[0]);
-                people.add(command[0]);
-            }else{
-                if(!teamExists){
-                    System.out.printf("Team %s does not exist!\n",command[1]);
-                }else {
-                    System.out.printf("Member %s cannot join team %s!\n",command[0],command[1]);
-                }
-            }
-
-            command=sc.nextLine().split("->");
+        for(Map.Entry<Double,Integer>entry: counts.entrySet()){
+            DecimalFormat df=new DecimalFormat("#.#######");
+            System.out.printf("%s -> %d%n",df.format(entry.getKey()),entry.getValue());
         }
-
-        //print teams
-        teams.sort(Comparator.comparing(Team::getSize).reversed().thenComparing(Team::getName));
-        List <String> removed=new ArrayList<>();
-        for (Team team:teams) {
-            if(team.getSize()<1){
-                removed.add(team.getName());
-            }
-        }
-
-        teams.removeIf(team -> team.getSize() < 1);
-        for (Team team:teams) {
-            System.out.printf("%s\n",team.getName());
-            System.out.printf("- %s\n",team.getCreator());
-
-            Collections.sort(team.getMembers());
-            for (String person:team.getMembers()) {
-                System.out.printf("-- %s\n",person);
-            }
-        }
-
-        System.out.println("Teams to disband:");
-        Collections.sort(removed);
-        for (String team:removed) {
-            System.out.println(team);
-        }
-
     }
 }
