71ed64126a641bfe13cbc6ce1a90b8052613f30f
Kristian <kristiian.donchev@abv.bg>
2023-11-01 14:39:17 +0200

    Maps, Lambda and Stream API - Exercise 1. Count Chars in a String

--- Main.java
+++ Main.java
@@ -6,11 +6,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String[] words= Arrays.stream(sc.nextLine().split(" "))
-                .filter(w ->w.length() % 2 == 0)
-                .toArray(String[]::new);
-        for (String el:words) {
-            System.out.println(el);
+        LinkedHashMap <Character,Integer>counts=new LinkedHashMap<>();
+        String text=sc.nextLine().replace(" ","");
+        for (int i = 0; i < text.length(); i++) {
+            char ch=text.charAt(i);
+
+            if(!counts.containsKey(ch))
+                counts.put(ch,1);
+            else
+                counts.put(ch,counts.get(ch)+1);
+
+        }
+
+        for (Map.Entry<Character,Integer>entry:counts.entrySet()){
+            System.out.printf("%c -> %d\n",entry.getKey(),entry.getValue());
         }
 
 
