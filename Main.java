c35d392a700e64137d3107428309d8a565d324dd
Kristian <kristiian.donchev@abv.bg>
2023-11-01 15:51:24 +0200

    Maps, Lambda and Stream API - Exercise 3. Orders

--- Main.java
+++ Main.java
@@ -6,28 +6,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        LinkedHashMap <String,Integer>counts=new LinkedHashMap<>();
-        String resource;
-        int quantity;
-        String command=sc.nextLine();
-        while(!command.equals("stop")){
-            resource=command;
-            quantity=Integer.parseInt(sc.nextLine());
+        LinkedHashMap <String,Double>counts=new LinkedHashMap<>();
+        LinkedHashMap <String,Integer> amount=new LinkedHashMap<>();
 
-            if(!counts.containsKey(resource))
-                counts.put(resource,quantity);
-            else
-                counts.put(resource,counts.get(resource)+quantity);
+        String [] command=sc.nextLine().split(" ");
+        while (!command[0].equals("buy")){
+            String product=command[0];
+            double price= Double.parseDouble(command[1]);
+            int quantity=Integer.parseInt(command[2]);
+
+            if(!counts.containsKey(product)){
+                counts.put(product,price);
+                amount.put(product,quantity);
+            }else {
+                counts.replace(product,price);
+                amount.put(product,amount.get(product)+quantity);
+            }
 
 
-            command=sc.nextLine();
+
+            command=sc.nextLine().split(" ");
         }
 
-        for (Map.Entry<String,Integer>entry: counts.entrySet()){
-            System.out.printf("%s -> %d\n",entry.getKey(),entry.getValue());
+
+
+
+        for (Map.Entry<String,Double>entry: counts.entrySet()){
+
+            for (Map.Entry<String,Integer>amm: amount.entrySet()) {
+                if(amm.getKey().equals(entry.getKey())) {
+                    double total = entry.getValue() * amm.getValue();
+                    System.out.printf("%s -> %.2f\n",entry.getKey(),total);
+                    break;
+                }
+
+            }
         }
-
-
 
     }
 }
