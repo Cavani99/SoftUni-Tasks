9fe11a18f17deb83e729642895a8e81169c210a4
Kristian <kristiian.donchev@abv.bg>
2023-11-02 14:35:23 +0200

    Maps, Lambda and Stream API - Exercise 7. Legendary Farming

--- Main.java
+++ Main.java
@@ -6,26 +6,53 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        LinkedHashMap <String,Double>students=new LinkedHashMap<>();
-        int number=Integer.parseInt(sc.nextLine());
-        for (int i = 1; i <=number ; i++) {
-            String name=sc.nextLine();
-            double grade=Double.parseDouble(sc.nextLine());
+        LinkedHashMap <String,Integer>materials=new LinkedHashMap<>();
 
-            if(!students.containsKey(name)){
-                students.put(name,grade);
-            }else{
-                students.put(name, (students.get(name)+grade)/2);
+        materials.put("shards",0);
+        materials.put("fragments",0);
+        materials.put("motes",0);
+
+        while(true) {
+
+            String []input=sc.nextLine().split(" ");
+            boolean LegendaryObtained=false;
+            for (int i = 0; i < input.length; i++) {
+                int value = Integer.parseInt(input[i]);
+                i++;
+                String material = input[i].toLowerCase();
+
+                if(!materials.containsKey(material)){
+                    materials.put(material,value);
+                }else{
+                    materials.put(material,materials.get(material)+value);
+                }
+
+                if(materials.get(material)>=250 && material.equals("shards")){
+                    System.out.println("Shadowmourne obtained!");
+                    materials.put(material,materials.get(material.toLowerCase())-250);
+                    LegendaryObtained=true;
+                    break;
+                }else if(materials.get(material)>=250 && material.equals("fragments")){
+                    System.out.println("Valanyr obtained!");
+                    materials.put(material,materials.get(material.toLowerCase())-250);
+                    LegendaryObtained=true;
+                    break;
+                }else if(materials.get(material)>=250 && material.equals("motes")){
+                    System.out.println("Dragonwrath obtained!");
+                    materials.put(material,materials.get(material.toLowerCase())-250);
+                    LegendaryObtained=true;
+                    break;
+                }
+
+
             }
-
-
+            if(LegendaryObtained)
+                break;
         }
 
+        for (Map.Entry<String,Integer>entry: materials.entrySet()){
 
-        for (Map.Entry<String,Double>entry: students.entrySet()){
-
-            if(entry.getValue()>=4.50)
-                System.out.printf("%s -> %.2f\n",entry.getKey(),entry.getValue());
+            System.out.printf("%s: %d\n",entry.getKey(),entry.getValue());
 
         }
 
