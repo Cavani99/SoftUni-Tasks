a3f38c30a2aa67a5b78236fb10b1cb73c8250702
Kristian <kristiian.donchev@abv.bg>
2023-11-08 14:43:00 +0200

    Text Processing - Lab 5. Digits, Letters and Other

--- Main.java
+++ Main.java
@@ -6,17 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String []banList=sc.nextLine().split(", ");
-        String text= sc.nextLine();
-        for (String word:banList) {
-
-            StringBuilder stars=new StringBuilder();
-            stars.append("*".repeat(word.length()));
-
-            while(text.contains(word)){
-                text=text.replace(word,stars);
-            }
+        String text=sc.nextLine();
+        StringBuilder digits=new StringBuilder();
+        StringBuilder letters=new StringBuilder();
+        StringBuilder symbols=new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            if(Character.isDigit(text.charAt(i))){
+                digits.append(text.charAt(i));
+            }else if(Character.isLetter(text.charAt(i))){
+                letters.append(text.charAt(i));
+            }else symbols.append(text.charAt(i));
         }
-        System.out.println(text);
+        System.out.println(digits);
+        System.out.println(letters);
+        System.out.println(symbols);
     }
 }
