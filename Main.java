92b6f9a8ebf662f70c86ec47466a536d7ebd2a10
Kristian <kristiian.donchev@abv.bg>
2023-11-08 22:27:40 +0200

    Text Processing - Exercise 1. Valid Usernames

--- Main.java
+++ Main.java
@@ -6,19 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String text=sc.nextLine();
-        StringBuilder digits=new StringBuilder();
-        StringBuilder letters=new StringBuilder();
-        StringBuilder symbols=new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-            if(Character.isDigit(text.charAt(i))){
-                digits.append(text.charAt(i));
-            }else if(Character.isLetter(text.charAt(i))){
-                letters.append(text.charAt(i));
-            }else symbols.append(text.charAt(i));
+        String [] text=sc.nextLine().split(", ");
+        for (String word: text) {
+            if(word.length()>=3 && word.length()<=16){
+                boolean isValid=true;
+                for (int i = 0; i < word.length(); i++) {
+                    char ch=word.charAt(i);
+                    if(Character.isLetter(ch) || Character.isDigit(ch) || ch=='-' || ch=='_') ;
+                    else {
+                        isValid=false;
+                        break;
+                    }
+                }
+                if(isValid)
+                    System.out.println(word);
+            }
+
         }
-        System.out.println(digits);
-        System.out.println(letters);
-        System.out.println(symbols);
+
     }
 }
