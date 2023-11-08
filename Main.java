b3e2fe39c22e5513f0f6b6db23b2fd1dba53c7af
Kristian <kristiian.donchev@abv.bg>
2023-11-08 22:40:02 +0200

    Text Processing - Exercise 2. Character Multiplier

--- Main.java
+++ Main.java
@@ -2,27 +2,28 @@ import java.util.*;
 
 public class Main {
 
+    public static int sumOfString(String text1,String text2,int sum){
+        for (int i = 0; i < text2.length(); i++) {
+
+            sum+=text1.charAt(i)*text2.charAt(i);
+        }
+        for (int i = text2.length(); i < text1.length(); i++) {
+            sum+=text1.charAt(i);
+        }
+
+        return sum;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String [] text=sc.nextLine().split(", ");
-        for (String word: text) {
-            if(word.length()>=3 && word.length()<=16){
-                boolean isValid=true;
-                for (int i = 0; i < word.length(); i++) {
-                    char ch=word.charAt(i);
-                    if(Character.isLetter(ch) || Character.isDigit(ch) || ch=='-' || ch=='_') ;
-                    else {
-                        isValid=false;
-                        break;
-                    }
-                }
-                if(isValid)
-                    System.out.println(word);
-            }
+        String [] text=sc.nextLine().split(" ");
+        int sum=0;
+        if(text[0].length()>=text[1].length())
+            System.out.println(sumOfString(text[0],text[1],sum));
+        else System.out.println(sumOfString(text[1],text[0],sum));
 
-        }
 
     }
 }
