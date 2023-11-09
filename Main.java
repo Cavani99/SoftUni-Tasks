b96ef44445c72e2c7b476e5d4241c4d7ab882e4a
Kristian <kristiian.donchev@abv.bg>
2023-11-09 12:53:12 +0200

    Text Processing - Exercise 6. Replace Repeating Chars

--- Main.java
+++ Main.java
@@ -6,42 +6,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String bigNumber=sc.nextLine().replaceFirst("^0+(?!$)", "");
-        int smallNumber=Integer.parseInt(sc.nextLine());
-        StringBuilder output=new StringBuilder();
-        int remains=0;
-        if(smallNumber==0){
-            System.out.println(0);
-        }else if(smallNumber==1){
-            System.out.println(bigNumber);
-        }else {
-            for (int i = bigNumber.length() - 1; i >= 0; i--) {
-                int result;
-                int digit = Integer.parseInt(String.valueOf(bigNumber.charAt(i)));
-                if (remains == 0) {
-                    result = smallNumber * digit;
-
-                    if (result >=10) {
-                        String[] split = String.valueOf(result).split("");
-                        remains = Integer.parseInt(split[0]);
-                        output.append(split[1]);
-                    } else output.append(result);
-                } else {
-                    result = smallNumber * digit + remains;
-                    remains = 0;
-
-                    if (result >= 10) {
-                        String[] split = String.valueOf(result).split("");
-                        remains = Integer.parseInt(split[0]);
-                        output.append(split[1]);
-                    } else output.append(result);
-                }
+        String text=sc.nextLine();
+        StringBuilder out=new StringBuilder();
+        char compare=' ';
+        for (int i = 0; i < text.length(); i++) {
+            if(i==0)
+                compare=text.charAt(i);
+            else if(text.charAt(i)!=compare){
+                out.append(compare);
+                compare=text.charAt(i);
             }
-            if (remains > 0)
-                output.append(remains);
-
-            System.out.println(output.reverse());
         }
+
+        out.append(compare);
+        System.out.println(out);
+
+
 
     }
 }
