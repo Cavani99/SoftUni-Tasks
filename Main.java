a33c6fa5f2304f8b5b98f5c0d062dd74f923aa88
Kristian <kristiian.donchev@abv.bg>
2023-11-09 14:59:28 +0200

    Text Processing - Exercise 8. Letters Change Numbers

--- Main.java
+++ Main.java
@@ -1,40 +1,60 @@
+import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
 
+    public static int findNumber(String text){
+        StringBuilder num=new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            if(Character.isDigit(text.charAt(i)))
+                num.append(text.charAt(i));
+        }
+
+        return Integer.parseInt(String.valueOf(num));
+    }
+
+    public static BigDecimal sum(String text, int number, BigDecimal sum){
+
+        boolean beforeNumber=true;
+        for (int i = 0; i < text.length(); i++) {
+            char ch=text.charAt(i);
+            int letterPos;
+            if(Character.isLetter(ch)){
+                if(beforeNumber ){
+                    if(Character.isUpperCase(ch)){
+                        letterPos=ch-64;
+                        sum=sum.add(BigDecimal.valueOf((double)number/letterPos));
+                    }
+                    else{
+                        letterPos=ch-96;
+                        sum=sum.add(BigDecimal.valueOf((double)number*letterPos));
+                    }
+                }else{
+                    if(Character.isUpperCase(ch)){
+                        letterPos=ch-64;
+                        sum=sum.subtract(BigDecimal.valueOf(letterPos));
+                    }
+                    else{
+                        letterPos=ch-96;
+                        sum=sum.add(BigDecimal.valueOf(letterPos));
+                    }
+                }
+            }else
+                beforeNumber=false;
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String text=sc.nextLine();
-        int explosionPower;
-        StringBuilder out=new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-
-            if(text.charAt(i)=='>'){
-                out.append(text.charAt(i));
-                i++;
-                explosionPower=Integer.parseInt(String.valueOf(text.charAt(i)));
-                while(explosionPower>0 && i<text.length()) {
-                    if(text.charAt(i)=='>'){
-                        out.append(text.charAt(i));
-                        i++;
-                        explosionPower+=Integer.parseInt(String.valueOf(text.charAt(i)));
-                    }else {
-                        i++;
-                        explosionPower--;
-                    }
-                }
-                i--;
-            }else {
-                out.append(text.charAt(i));
-            }
-
-
+        String []info=sc.nextLine().split("\\s+");
+        int number1;
+        BigDecimal sum = BigDecimal.valueOf(0);
+        for (int i = 0; i < info.length; i++) {
+            number1=findNumber(info[i]);
+            sum=sum(info[i], number1,sum);
         }
-
-        System.out.println(out);
-
-
+        System.out.printf("%.2f",sum);
     }
 }
