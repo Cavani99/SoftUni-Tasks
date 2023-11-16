6f998eb264477396259191c689d29cefc22e62b6
Kristian <kristiian.donchev@abv.bg>
2023-11-16 14:50:40 +0200

    Regular Expressions - Exercises 2. SoftUni Bar Income

--- Main.java
+++ Main.java
@@ -8,24 +8,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String regex="(>>)(?<furniture>\\w+)(<<)(?<price>\\d+[.]?\\d+)!(?<quantity>\\d+)\\b";
+        String regex="%(?<name>[A-Z][a-z]+)%[^|$.%]*<(?<product>\\w+)>[^|$.%]*\\|(?<count>\\d+)\\|[^|$.%]*?(?<price>\\d+[.]?\\d+)\\$";
 
         Pattern pattern= Pattern.compile(regex);
 
         double sum=0;
         String input=sc.nextLine();
-
-        System.out.println("Bought furniture:");
-        while(!input.equals("Purchase")){
+        while(!input.equals("end of shift")){
             Matcher matcher=pattern.matcher(input);
             if(matcher.find()){
-                System.out.printf("%s\n",matcher.group("furniture"));
-                sum+=Double.parseDouble(matcher.group("price"))*Integer.parseInt(matcher.group("quantity"));
+                System.out.printf("%s: %s - %.2f\n",matcher.group("name"),matcher.group("product"),
+                        Integer.parseInt(matcher.group("count"))*Double.parseDouble(matcher.group("price")));
+                sum+=Double.parseDouble(matcher.group("price"))*Integer.parseInt(matcher.group("count"));
             }
 
             input=sc.nextLine();
         }
-        System.out.printf("Total money spend: %.2f",sum);
+        System.out.printf("Total income: %.2f",sum);
 
     }
 }
