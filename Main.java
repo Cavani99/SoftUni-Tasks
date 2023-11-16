bd22f60d0e1df5e3598f1901fddd5f5ae6b93195
Kristian <kristiian.donchev@abv.bg>
2023-11-16 15:45:08 +0200

    Regular Expressions - Exercises 3. Star Enigma

--- Main.java
+++ Main.java
@@ -8,23 +8,50 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String regex="%(?<name>[A-Z][a-z]+)%[^|$.%]*<(?<product>\\w+)>[^|$.%]*\\|(?<count>\\d+)\\|[^|$.%]*?(?<price>\\d+[.]?\\d+)\\$";
 
-        Pattern pattern= Pattern.compile(regex);
+        int number=Integer.parseInt(sc.nextLine());
+        ArrayList <String> attacked=new ArrayList<>();
+        ArrayList <String> destroyed=new ArrayList<>();
 
-        double sum=0;
-        String input=sc.nextLine();
-        while(!input.equals("end of shift")){
+        for (int i = 0; i < number; i++) {
+            String input=sc.nextLine();
+
+            String regex="[star]*[STAR]*";
+            Pattern pattern= Pattern.compile(regex);
             Matcher matcher=pattern.matcher(input);
-            if(matcher.find()){
-                System.out.printf("%s: %s - %.2f\n",matcher.group("name"),matcher.group("product"),
-                        Integer.parseInt(matcher.group("count"))*Double.parseDouble(matcher.group("price")));
-                sum+=Double.parseDouble(matcher.group("price"))*Integer.parseInt(matcher.group("count"));
+            String len="";
+            while(matcher.find()) {
+                len+= matcher.group();
             }
+            int size=len.length();
 
-            input=sc.nextLine();
+            StringBuilder decrypted= new StringBuilder();
+            for (int j = 0; j < input.length(); j++) {
+                char ch=input.charAt(j);
+                ch-=size;
+                decrypted.append(ch);
+            }
+            regex="[^@\\-!:>]*@(?<planet>[A-Z][a-z]*)[^@\\-!:>]*:(?<population>\\d*)[^@\\-!:>]*!(?<event>[ADad]{1})![^@\\-!:>]*->(?<soldiers>\\d+)[^@\\-!:>]*";
+            pattern= Pattern.compile(regex);
+            matcher=pattern.matcher(decrypted);
+            if(matcher.find()){
+                if(matcher.group("event").equals("A")){
+                    attacked.add(matcher.group("planet"));
+                }else
+                    destroyed.add(matcher.group("planet"));
+            }
         }
-        System.out.printf("Total income: %.2f",sum);
+        Collections.sort(attacked);
+        Collections.sort(destroyed);
+        System.out.printf("Attacked planets: %d\n",attacked.size());
+        for (String planet:attacked) {
+            System.out.printf("-> %s\n",planet);
+        }
+        System.out.printf("Destroyed planets: %d\n",destroyed.size());
+        for (String planet:destroyed) {
+            System.out.printf("-> %s\n",planet);
+        }
+
 
     }
 }
